eea973b59d29653936a43d9cfee902df830c01a7
DJHeadset <zoltan.blaho.83@gmail.com>
2023-12-10 23:34:23 +0100

    map frontend

--- frontend/src/Components/Map.jsx
+++ frontend/src/Components/Map.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading/Loading";
 
 
 function Map() {
+  const [cityList, setCityList] = useState([])
+  const [loading, setLoading] = useState(true)
   
+  const fetchCityData = () => {
+    return fetch("https://pokeapi.co/api/v2/location")
+    .then((res) => res.json()
+    .then((names) => names.results.map((e) => {return e.name}))
+    );
+  };
+
+  useEffect(() => {
+    fetchCityData().then((cities) => {
+      setLoading(false);
+      setCityList(cities);
+    });
+  }, []);
   
-  
-  return (
+  if (loading) {
+    return <Loading />;
+  } else if (cityList) {
+    return (
     <>
-      <div>Map</div>
+      <div className="location-name">
+        {cityList.map((city, i) => (
+          <h2 key={i}>
+            <div>{city}</div>
+          </h2>
+        ))}
+      </div>
     </>
   );
+    }
 }
 
 export default Map;
